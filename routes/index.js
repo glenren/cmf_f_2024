b47dd7c61b128c80f4e1fb3c7da96cce7159eebe
Silvana Huang <silvanahuang23@gmail.com>
2024-03-09 18:16:23 -0800

    converted index.html file generated from figma prototype into index.moustache file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { pageTitle: "Page Title", appName: "app Name", communityLevel: 10 });
+});
+router.get('/list', function(req, res, next) {
+  res.render('list', { pageTitle: 'Express' });
+});
+router.get('/events/:eventId', function(req, res, next) {
+  res.render('event', { pageTitle: 'Express' });
 });
 
 module.exports = router;
